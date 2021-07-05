e914dc2a8ca0f51c6195db081dd2d577a6e459cb
Mychi Darko <mickdd22@gmail.com>
2021-07-05 11:06:57 +0000

    :art: added stats for topnav section

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -2,12 +2,12 @@ import Stats from "./components/Stats";
 import TopNav from "./components/TopNav";
 import FadeIn from "react-fade-in";
 
-const Main: React.FC<any> = ({ children, page, className }) => {
+const Main: React.FC<any> = ({ children, page, className, headerTitle, headerContent }) => {
     return (
         <section className={`main-layout ${page.split(" ").join("-").toLowerCase()} ${className}`}>
             <header>
                 <TopNav />
-                <Stats />
+                <Stats title={headerTitle} content={headerContent}  />
             </header>
             <FadeIn className="main-layout__content">
                 {children}
--- src/layouts/components/Stats.tsx
+++ src/layouts/components/Stats.tsx
@@ -1,43 +1,73 @@
 import { showOnly } from "@/utils/app";
 import { Link } from "glass-router";
 import GlassX from "glassx";
+import FadeIn from "react-fade-in";
 
-const Stats = () => {
+const Stats: React.FC<any> = ({ title, content }) => {
+    const defaultTitle = {
+        lead: "Welcome",
+        text: showOnly(17, GlassX.get("user")?.name || ""),
+        extra: "Admin",
+    };
+
+    const defaultContent = [
+        {
+            lead: "Processed Orders",
+            text: "34"
+        },
+        {
+            lead: "Pending Orders",
+            text: "4"
+        },
+        {
+            lead: "Total Users Registered",
+            text: "300"
+        },
+        {
+            lead: "Total amount sold",
+            text: "GHC 34,987"
+        },
+        {
+            lead: "Total amount bought",
+            text: "GHC 300"
+        },
+    ];
+
+    if (!title) {
+        title = defaultTitle;
+    }
+
+    if (!content) {
+        content = defaultContent;
+    }
+
     return (
-        <div className="page-stats flex flex:center-between pt:_3 pb:_6 px:10">
+        <div className="page-stats flex pt:_3 pb:_6 px:10">
             <div className="welcome__details w:25">
-                <small>Welcome</small>
-                <h3>{showOnly(17, GlassX.get("user")?.name || "")}</h3>
-                <h5>Admin</h5>
+                {title && (
+                    <>
+                        <small>{title.lead}</small>
+                        <h3>{title.text}</h3>
+                        {title.extra && <h5>{title.extra}</h5>}
+                    </>
+                )}
             </div>
-            <div className="details__container flex flex:center-between w:100 position:relative">
-                <div className="details">
-                    <p className="processed">Processed Orders</p>
-                    <h3>34</h3>
-                </div>
-                <div className="details">
-                    <p className="pending">Pending Orders</p>
-                    <h3>3</h3>
-                </div>
-                <div className="details">
-                    <p className="total-users">Total Users Registered</p>
-                    <h3>300</h3>
-                </div>
-                <div className="details">
-                    <p className="total-amount-sold">Total amount sold</p>
-                    <h3 className="amount">GHC 34,987</h3>
-                </div>
-                <div className="details">
-                    <p className="total-amount-bought">Total amount bought</p>
-                    <h3 className="amount">GHC 300</h3>
-                </div>
-                <Link
-                    to={{ name: "reports" }}
-                    className="position:absolute right:0 view-reports"
-                >
-                    View all reports
-                </Link>
-            </div>
+            <FadeIn className="details__container flex flex:center-between w:100 position:relative">
+                {content && content.map(({ lead, text }: any, index: number) => (
+                    <div className="details" key={lead}>
+                        <p>{lead}</p>
+                        <h3>{text}</h3>
+                    </div>
+                ))}
+                {!title && (
+                    <Link
+                        to={{ name: "reports" }}
+                        className="position:absolute right:0 view-reports"
+                    >
+                        View all reports
+                    </Link>
+                )}
+            </FadeIn>
         </div>
     );
 }
