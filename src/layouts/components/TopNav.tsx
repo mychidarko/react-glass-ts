f3a5cc588da2d4759fb5140ca85c4ee7bb9db2ea
Mychi Darko <mickdd22@gmail.com>
2021-07-05 11:08:55 +0000

    :lipstick: updated logout button styles

--- src/layouts/components/TopNav.tsx
+++ src/layouts/components/TopNav.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import { Link, useRoute } from "glass-router";
 import GlassX from "glassx";
 
@@ -24,7 +25,7 @@ const TopNav = () => {
                 <Link to={{ name: "dashboard" }}>Home</Link>
                 <div className="ml:_3">Search</div>
                 <div style={{ flex: 100 }}></div>
-                <button onClick={handleLogout}>Logout</button>
+                <Button color="transparent" onClick={handleLogout}>Log Out</Button>
             </div>
         </nav>
     );
